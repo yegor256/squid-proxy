ca03ae02d7f56ac1d5455292f8348b3079a0bf9c
Yegor Bugayenko <yegor256@gmail.com>
2019-08-02 18:23:19 +0300

    chec

--- entry.sh
+++ entry.sh
@@ -3,6 +3,15 @@
 set -x
 set -e
 
+if [ -z "${USERNAME}" ]; then
+  echo "You have to specify the -e USERNAME=... argument"
+  exit -1
+fi
+if [ -z "${PASSWORD}" ]; then
+  echo "You have to specify the -e PASSWORD=... argument"
+  exit -1
+fi
+
 htpasswd -c /etc/squid/passwd "${USERNAME}" "${PASSWORD}"
 
 /etc/init.d/squid start
