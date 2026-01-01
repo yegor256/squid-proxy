837f58b8c33e0480cf2875906dbfdcf1e1c9c2ee
Yegor Bugayenko <yegor256@gmail.com>
2026-01-01 13:10:27 +0300

    year up to 2026

--- entry.sh
+++ entry.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# SPDX-FileCopyrightText: Copyright (c) 2019-2025 Yegor Bugayenko
+# SPDX-FileCopyrightText: Copyright (c) 2019-2026 Yegor Bugayenko
 # SPDX-License-Identifier: MIT
 
 set -x
